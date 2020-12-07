7c78e007da4dfeeb09a739319164b25bd9ef4771
Thiago Fernandes <thiago.fernandes@tqi.com.br>
2020-12-07 10:35:10 -0300

    ios Project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 import 'app/app_module.dart';
 
-void main() => runApp(ModularApp(module: AppModule()));
+void main() async {
+  OneSignal.shared.setLogLevel(OSLogLevel.verbose, OSLogLevel.none);
+
+  OneSignal.shared.init("f05be84a-3328-467a-863d-3b222d3dbeeb",
+      iOSSettings: {OSiOSSettings.autoPrompt: false, OSiOSSettings.inAppLaunchUrl: false});
+  OneSignal.shared.setInFocusDisplayType(OSNotificationDisplayType.notification);
+
+// The promptForPushNotificationsWithUserResponse function will show the iOS push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission
+  await OneSignal.shared.promptUserForPushNotificationPermission(fallbackToSettings: true);
+  runApp(ModularApp(module: AppModule()));
+}
