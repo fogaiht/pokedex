9819fe53aa6d1a39a7b975c64f9f1f954b107ff9
Thiago Fernandes <thiagofpf@gmail.com>
2020-12-22 23:52:16 -0300

    deleted some unused test files

--- test/app/modules/home/home_page_test.dart
+++ test/app/modules/home/home_page_test.dart
@@ -5,7 +5,7 @@ import 'package:poke_api/app/modules/home/home_page.dart';
 
 main() {
   testWidgets('HomePage has title', (WidgetTester tester) async {
-    await tester.pumpWidget(buildTestableWidget(HomePage(title: 'Home')));
+    await tester.pumpWidget(buildTestableWidget(HomePage()));
     final titleFinder = find.text('Home');
     expect(titleFinder, findsOneWidget);
   });
